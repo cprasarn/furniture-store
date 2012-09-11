c19dc895a772a6245aa5092408f663db5acf94f6
Cho Prasarn <jodhiroj@gmail.com>
2012-09-11 10:31:13 -0500

    Update database schema

--- saas/db/schema.rb
+++ saas/db/schema.rb
@@ -14,41 +14,41 @@
 ActiveRecord::Schema.define(:version => 0) do
 
   create_table "address", :force => true do |t|
-    t.string   "street1",     :limit => 50,                :null => false
+    t.string   "street1",     :limit => 50, :null => false
     t.string   "street2",     :limit => 50
-    t.string   "city",        :limit => 50,                :null => false
-    t.string   "state",       :limit => 5,                 :null => false
-    t.string   "zip_code",    :limit => 20,                :null => false
-    t.datetime "create_date",                              :null => false
+    t.string   "city",        :limit => 50, :null => false
+    t.string   "state",       :limit => 5,  :null => false
+    t.string   "zip_code",    :limit => 20, :null => false
+    t.datetime "create_date",               :null => false
     t.datetime "modify_date"
-    t.integer  "status",      :limit => 1,  :default => 1, :null => false
+    t.integer  "status",      :limit => 1,  :null => false
   end
 
   create_table "customers", :force => true do |t|
-    t.string   "name",           :limit => 80,                :null => false
+    t.string   "name",           :limit => 80, :null => false
     t.string   "home_phone",     :limit => 20
     t.string   "mobile_phone",   :limit => 20
     t.string   "business_phone", :limit => 20
     t.string   "fax",            :limit => 20
     t.string   "email",          :limit => 80
-    t.datetime "create_date",                                 :null => false
+    t.datetime "create_date",                  :null => false
     t.datetime "modify_date"
-    t.integer  "status",         :limit => 1,  :default => 1, :null => false
+    t.integer  "status",         :limit => 1,  :null => false
   end
 
   create_table "customers_address", :force => true do |t|
-    t.integer  "customer_id", :limit => 3,                  :null => false
-    t.string   "address_id",  :limit => 128,                :null => false
-    t.integer  "is_primary",  :limit => 1,   :default => 1, :null => false
-    t.datetime "create_date",                               :null => false
-    t.datetime "modify_date",                               :null => false
-    t.integer  "status",      :limit => 1,   :default => 1, :null => false
+    t.integer  "customer_id", :limit => 3,                 :null => false
+    t.string   "address_id",  :limit => 40,                :null => false
+    t.integer  "is_primary",  :limit => 1,  :default => 1, :null => false
+    t.datetime "create_date",                              :null => false
+    t.datetime "modify_date",                              :null => false
+    t.integer  "status",      :limit => 1,                 :null => false
   end
 
   create_table "items", :force => true do |t|
     t.string   "order_number",   :limit => 10,                 :null => false
     t.string   "item_number",    :limit => 10,                 :null => false
-    t.integer  "quantity",       :limit => 1,   :default => 0
+    t.integer  "quantity",       :limit => 1
     t.string   "wood",           :limit => 100
     t.string   "finish"
     t.string   "description"
@@ -63,18 +63,29 @@ ActiveRecord::Schema.define(:version => 0) do
   add_index "items", ["order_number"], :name => "FK_items_orders"
 
   create_table "ledger", :force => true do |t|
-    t.string   "order_number",   :limit => 50, :default => "0", :null => false
-    t.string   "payment_type",   :limit => 20,                  :null => false
-    t.string   "payment_method", :limit => 20,                  :null => false
-    t.float    "amount",                       :default => 0.0, :null => false
-    t.datetime "payment_date",                                  :null => false
-    t.integer  "status",         :limit => 1,  :default => 1,   :null => false
+    t.string   "order_number",   :limit => 50, :null => false
+    t.string   "payment_type",   :limit => 20, :null => false
+    t.string   "payment_method", :limit => 20, :null => false
+    t.float    "amount",                       :null => false
+    t.datetime "payment_date",                 :null => false
+    t.integer  "status",         :limit => 1,  :null => false
   end
 
+  create_table "note", :force => true do |t|
+    t.string   "order_number", :limit => 20,  :null => false
+    t.string   "note_type",    :limit => 40,  :null => false
+    t.text     "content",      :limit => 255, :null => false
+    t.datetime "create_date",                 :null => false
+    t.datetime "modify_date",                 :null => false
+    t.integer  "status",       :limit => 1,   :null => false
+  end
+
+  add_index "note", ["order_number"], :name => "FK_note_orders"
+
   create_table "orders", :force => true do |t|
-    t.string   "order_number",    :limit => 10,                 :null => false
-    t.integer  "customer_id",     :limit => 3,                  :null => false
-    t.string   "address_id",      :limit => 128,                :null => false
+    t.string   "order_number",    :limit => 10,  :null => false
+    t.integer  "customer_id",     :limit => 3,   :null => false
+    t.string   "address_id",      :limit => 128, :null => false
     t.string   "lead_source"
     t.string   "delivery_option", :limit => 80
     t.float    "price"
@@ -82,23 +93,58 @@ ActiveRecord::Schema.define(:version => 0) do
     t.float    "discount"
     t.float    "delivery_charge"
     t.float    "sales_tax"
-    t.datetime "order_date",                                    :null => false
-    t.integer  "estimated_time",  :limit => 1,                  :null => false
+    t.datetime "order_date",                     :null => false
+    t.integer  "estimated_time",  :limit => 1,   :null => false
     t.date     "delivery_date"
-    t.integer  "status",          :limit => 1,   :default => 1, :null => false
+    t.integer  "status",          :limit => 1,   :null => false
   end
 
   add_index "orders", ["order_number"], :name => "order_number", :unique => true
 
-  create_table "orders_number", :id => false, :force => true do |t|
-    t.string  "branch", :limit => 50, :null => false
-    t.string  "code",   :limit => 10, :null => false
-    t.integer "value",  :limit => 2,  :null => false
+  create_table "payment_type", :primary_key => "name", :force => true do |t|
+    t.string "description", :limit => 40, :null => false
+  end
+
+  create_table "role", :force => true do |t|
+    t.string "name", :limit => 50, :null => false
   end
 
   create_table "states", :id => false, :force => true do |t|
     t.string "name",         :limit => 50, :null => false
     t.string "abbreviation", :limit => 2,  :null => false
   end
+
+  create_table "stores", :id => false, :force => true do |t|
+    t.string  "name",         :limit => 50, :null => false
+    t.string  "code",         :limit => 10, :null => false
+    t.integer "store_number", :limit => 2,  :null => false
+    t.string  "street1",      :limit => 40, :null => false
+    t.string  "street2",      :limit => 40, :null => false
+    t.string  "city",         :limit => 40, :null => false
+    t.string  "state",        :limit => 5,  :null => false
+    t.string  "zip_code",     :limit => 10, :null => false
+    t.string  "phone",        :limit => 20, :null => false
+    t.string  "fax",          :limit => 20, :null => false
+    t.integer "order_number", :limit => 2,  :null => false
+    t.float   "sales_tax",                  :null => false
+  end
+
+  create_table "user", :force => true do |t|
+    t.string   "name",        :limit => 40, :null => false
+    t.datetime "create_date",               :null => false
+    t.datetime "modify_date",               :null => false
+    t.integer  "status",      :limit => 1,  :null => false
+  end
+
+  create_table "user_role", :force => true do |t|
+    t.string   "user_id",     :limit => 40, :null => false
+    t.integer  "role_id",     :limit => 1,  :null => false
+    t.datetime "create_date",               :null => false
+    t.datetime "modify_date",               :null => false
+    t.integer  "status",      :limit => 1,  :null => false
+  end
+
+  add_index "user_role", ["role_id"], :name => "FK_user_role_role"
+  add_index "user_role", ["user_id"], :name => "FK_user_role_user"
 
 end
